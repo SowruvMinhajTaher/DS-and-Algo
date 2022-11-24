ccbdad394dcf0b09771ee0151ac5539d59f7b936
Sowruv Minhaj Taher Titu <72273710+SowruvMinhajTaher@users.noreply.github.com>
2022-11-24 07:27:55 +0600

    Time: 64 ms (58.83%), Space: 14.2 MB (25.26%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,5 +1,6 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
+        '''
         strSet = {s for s in nums} #hashset
         k = len(nums)
         def dfs(comb):
@@ -16,3 +17,20 @@ class Solution:
                 comb.pop()
             
         return dfs([])
+        '''
+        strSet = {s for s in nums}
+        def backtrack(i, cur): #index, currentString
+            if i == len(nums):
+                res = "".join(cur) 
+                # as we converted string as list, so we are converting back to str
+                return None if res in strSet else res
+            res = backtrack(i+1, cur) #choosing 0 as we given it by default
+            if res:
+                return res
+            
+            cur[i] = "1"#choosing 1 
+            res = backtrack(i+1, cur)
+            if res:
+                return res
+            
+        return backtrack(0,["0" for s in nums]) #converting into list as string is immutable in python
