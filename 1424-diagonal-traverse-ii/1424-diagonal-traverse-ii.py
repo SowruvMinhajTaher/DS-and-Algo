1034a716fb1f7d310797ecef9632f5d029db3c93
Sowruv Minhaj Taher Titu <72273710+SowruvMinhajTaher@users.noreply.github.com>
2023-11-22 20:38:58 +0600

    Time: 787 ms (63.32%), Space: 82.1 MB (5.02%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -1,7 +1,15 @@
+# class Solution:
+#     def findDiagonalOrder(self, A: List[List[int]]) -> List[int]:
+#         d = defaultdict(list)
+#         for i in range(len(A)):
+#             for j in range(len(A[i])):
+#                 d[i+j].append(A[i][j])
+#         return [v for k in d.keys() for v in reversed(d[k])]
+
 class Solution:
-    def findDiagonalOrder(self, A: List[List[int]]) -> List[int]:
-        d = defaultdict(list)
-        for i in range(len(A)):
-            for j in range(len(A[i])):
-                d[i+j].append(A[i][j])
-        return [v for k in d.keys() for v in reversed(d[k])]
+    def findDiagonalOrder(self, nums):
+        diagonals = [deque() for _ in range(len(nums) + max(len(row) for row in nums) - 1)]
+        for row_id, row in enumerate(nums):
+            for col_id in range(len(row)):
+                diagonals[row_id + col_id].appendleft(row[col_id])
+        return list(chain(*diagonals))
