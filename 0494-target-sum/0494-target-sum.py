71a122271be8e7c42a887292a220e879eac61445
Sowruv Minhaj Taher Titu <72273710+SowruvMinhajTaher@users.noreply.github.com>
2024-12-26 23:49:44 +0600

    Time: 207 ms (18.20%), Space: 18.2 MB (59.59%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums, S):
+        index = len(nums) - 1
+        curr_sum = 0
+        self.memo = {}
+        return self.dp(nums, S, index, curr_sum)
+        
+    def dp(self, nums, target, index, curr_sum):
+        if (index, curr_sum) in self.memo:
+            return self.memo[(index, curr_sum)]
+        
+        if index < 0 and curr_sum == target:
+            return 1
+        if index < 0:
+            return 0 
+        
+        positive = self.dp(nums, target, index-1, curr_sum + nums[index])
+        negative = self.dp(nums, target, index-1, curr_sum + -nums[index])
+        
+        self.memo[(index, curr_sum)] = positive + negative
+        return self.memo[(index, curr_sum)]
